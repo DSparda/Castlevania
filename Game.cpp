cda4ac279b8dc49df586e4c4ddf4f689582fe5da
DSparda <DSparda@users.noreply.github.com>
2019-10-22 07:24:37 +0700

    Game Init and Draw Function

---
+++ Game.cpp
@@ -0,0 +1,62 @@
+#include "Game.h"
+
+CGame* CGame::__instance = NULL;
+
+void CGame::Init(HWND hWnd)
+{
+	d3d = Direct3DCreate9(D3D_SDK_VERSION);
+
+	this->hWnd = hWnd;
+
+	D3DPRESENT_PARAMETERS d3dpp;
+
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
+
+	d3dpp.Windowed = TRUE;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.BackBufferFormat = D3DFMT_X8R8G8B8;
+	d3dpp.BackBufferCount = 1;
+
+	RECT r;
+	GetClientRect(hWnd, &r);
+
+	d3dpp.BackBufferHeight = r.bottom + 1;
+	d3dpp.BackBufferWidth = r.right + 1;
+
+	d3d->CreateDevice(
+		D3DADAPTER_DEFAULT,
+		D3DDEVTYPE_HAL,
+		hWnd,
+		D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+		&d3dpp,
+		&d3ddv);
+
+	d3ddv->GetBackBuffer(0, 0, D3DBACKBUFFER_TYPE_MONO, &backBuffer);
+
+	D3DXCreateSprite(d3ddv, &spriteHandler);
+}
+
+void CGame::Draw(float x, float y, LPDIRECT3DTEXTURE9 texture, int left, int top, int right, int bottom)
+{
+	D3DXVECTOR3 p(x, y, 0);
+	RECT r;
+	r.left = left;
+	r.top = top;
+	r.right = right;
+	r.bottom = bottom;
+	spriteHandler->Draw(texture, &r, NULL, &p, D3DCOLOR_XRGB(255, 255, 255));
+}
+
+CGame::~CGame()
+{
+	if (d3d != NULL) d3d->Release();
+	if (d3ddv != NULL) d3ddv->Release();
+	if (backBuffer != NULL) backBuffer->Release();
+	if (spriteHandler != NULL) spriteHandler->Release();
+}
+
+CGame* CGame::GetInstance()
+{
+	if (__instance == NULL) __instance = new CGame();
+	return __instance;
+}
